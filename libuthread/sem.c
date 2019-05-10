8d43b61b50a52fa17b18ce816e9036ee5680929d
Nathan Hoffman <nathanahoffman@gmail.com>
2019-05-09 20:50:44 -0700

    sem_create()

--- libuthread/sem.c
+++ libuthread/sem.c
@@ -7,11 +7,17 @@
 
 struct semaphore {
 	/* TODO Phase 1 */
+  size_t count;
 };
 
 sem_t sem_create(size_t count)
 {
 	/* TODO Phase 1 */
+  sem_t new_sem;
+  new_sem = (sem_t)malloc(sizeof(semaphore));
+
+  new_sem->count = count;
+  return new_sem;
 }
 
 int sem_destroy(sem_t sem)
@@ -33,4 +39,3 @@ int sem_getvalue(sem_t sem, int *sval)
 {
 	/* TODO Phase 1 */
 }
-
