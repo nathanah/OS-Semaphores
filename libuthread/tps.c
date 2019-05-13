c866d21ad1e850df007f7aaa5ec57bcc88a6009f
Nathan Hoffman <nathanahoffman@gmail.com>
2019-05-13 01:15:10 -0700

    tps_destroy()

--- libuthread/tps.c
+++ libuthread/tps.c
@@ -18,13 +18,13 @@
 /* TODO: Phase 2 */
 struct page
 }
-  void *address
+  void *address;
 }
-typedef struct page *page_t
+typedef struct page *page_t;
 
 struct TPS {
   pthread_t TID;
-  struct pageHandler *page
+  struct pageHandler *page;
 }
 typedef struct TPS *tps_t;
 
@@ -39,31 +39,59 @@ int tps_create(void)
 {
 	/* TODO: Phase 2 */
   tps_t currTPS = (tps_t)malloc(sizeof(struct TPS));
-  if !(currTPS) {
+
+  if (!currTPS) {
     return -1;
   }
+
   currTPS->TID = pthread_self();
   page_t page = malloc(sizeofstruct(page));
-  if !(page)
+  if (!page)
     return 1;
+
   page->address = mmap(NULL, TPS_SIZE, PROT_NONE, MAP_PRIVATE | MAP_ANONYMOUS, FD, OFFSET);
   currTPS->page = page;
   queue_enqueue(tpsHolders, currTPS);
+
+  return 0;
+}
+
+//queue_iterate function to return tps with given tid
+int tps_find(tps_t tps, pthread_t tid){
+  if(tps->tid == tid){
+    return 1;
+  }
+  else
+    return 0;
 }
 
 int tps_destroy(void)
 {
 	/* TODO: Phase 2 */
+  //get tps for current thread
+  tps_t tps;
+  if(queue_iterate(tpsHolders, tps_find, pthread_self(), &tps) == -1){
+    //return -1 if no tps for this tid
+    return -1;
+  }
+
+  //delete tps stuff
+  munmap(tps->page->address, TPS_SIZE);
+  free(tps);
+
+  return 0;
 }
 
 int tps_read(size_t offset, size_t length, char *buffer)
 {
 	/* TODO: Phase 2 */
+
 }
 
 int tps_write(size_t offset, size_t length, char *buffer)
 {
 	/* TODO: Phase 2 */
+
 }
 
 int tps_clone(pthread_t tid)
