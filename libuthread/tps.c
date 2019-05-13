752f008a15f144819a4ed9e728cef853f509aef7
Hanapina <jewli@ucdavis.edu>
2019-05-13 00:31:47 -0700

    Started tps?

--- libuthread/tps.c
+++ libuthread/tps.c
@@ -12,7 +12,23 @@
 #include "thread.h"
 #include "tps.h"
 
+#define FD -1
+#define OFFSET 0
+
 /* TODO: Phase 2 */
+struct page
+}
+  void *address
+}
+typedef struct page *page_t
+
+struct TPS {
+  pthread_t TID;
+  struct pageHandler *page
+}
+typedef struct TPS *tps_t;
+
+queue_t tpsHolders;
 
 int tps_init(int segv)
 {
@@ -22,6 +38,17 @@ int tps_init(int segv)
 int tps_create(void)
 {
 	/* TODO: Phase 2 */
+  tps_t currTPS = (tps_t)malloc(sizeof(struct TPS));
+  if !(currTPS) {
+    return -1;
+  }
+  currTPS->TID = pthread_self();
+  page_t page = malloc(sizeofstruct(page));
+  if !(page)
+    return 1;
+  page->address = mmap(NULL, TPS_SIZE, PROT_NONE, MAP_PRIVATE | MAP_ANONYMOUS, FD, OFFSET);
+  currTPS->page = page;
+  queue_enqueue(tpsHolders, currTPS);
 }
 
 int tps_destroy(void)
@@ -43,4 +70,3 @@ int tps_clone(pthread_t tid)
 {
 	/* TODO: Phase 2 */
 }
-
