c11706c8db9221d43c8452baa783e158a171a6ce
Nathan Hoffman <nathanahoffman@gmail.com>
2019-05-14 22:03:37 -0700

    fixed tpsPt2 bugs

--- test/tpsPt2.c
+++ test/tpsPt2.c
@@ -26,13 +26,13 @@ void *thread1 (void *arg) {
   assert(tps_write(5000, 1000, str) == -1);
   assert(tps_read(5000, 1000, str) == -1);
 
-  fprintf(stdout, "I passed the tests!\n")
+  fprintf(stdout, "I passed the tests!\n");
   return 0;
 }
 
 int main (int argc, char **argv) {
   pthread_t tid;
-  assert(tsp_init(1) == 0);
+  assert(tps_init(1) == 0);
   pthread_create(&tid, NULL, thread1, NULL);
   pthread_join(tid, NULL);
   return 0;
