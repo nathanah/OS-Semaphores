32fde8490c8bc45c2bb211b560029cc4c40a3a0a
Nathan Hoffman <nathanahoffman@gmail.com>
2019-05-14 20:49:06 -0700

    page_t

--- libuthread/tps.c
+++ libuthread/tps.c
@@ -225,7 +225,7 @@ int tps_create_with_pointer(tps_t tps)
   printf("after if\n");
   currTPS->tid = pthread_self();
   printf("almost malloc2\n");
-  page_t page = malloc(sizeof(struct page));
+  page_t page = (page_t)malloc(sizeof(struct page));
   if (page == NULL){
     printf("malloc2 error\n");
     return -1;
