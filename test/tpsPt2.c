d4ff865f380e042f43b1330473ee16b13f17c2a2
Hanapina <jewli@ucdavis.edu>
2019-05-14 21:59:40 -0700

    Oops

--- test/tpsPt2.c
+++ test/tpsPt2.c
@@ -9,6 +9,31 @@
 
 void *thread1 (void *arg) {
   char str[1000] = {0};
-  assert(tps_write(0, 1000, str) == -1)
-  
+
+  //no init space
+  assert(tps_write(0, 1000, str) == -1);
+  assert(tps_read(0, 1000, str) == -1);
+
+  //Test double create
+  assert(tps_create() == 0);
+  assert(tps_create() == -1);
+
+  //Test double destroy
+  assert(tps_destroy() == 0);
+  assert(tps_destroy() == -1);
+
+  //Read and write out of bounds
+  assert(tps_write(5000, 1000, str) == -1);
+  assert(tps_read(5000, 1000, str) == -1);
+
+  fprintf(stdout, "I passed the tests!\n")
+  return 0;
+}
+
+int main (int argc, char **argv) {
+  pthread_t tid;
+  assert(tsp_init(1) == 0);
+  pthread_create(&tid, NULL, thread1, NULL);
+  pthread_join(tid, NULL);
+  return 0;
 }
